81463f82bbbb71001cf56967b00876544e45dde4
Sagara222 <sagaranipun20@gmail.com>
2025-11-21 08:24:24 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '36cc0fce89936091' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '46253dc51e203869' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
